28e415d61a1bfd4b85440f39dd7bf11701253c0c
Joshua Andes <andesjoshua@comcast.net>
2021-09-25 12:03:57 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const newTaskForm = document.getElementById('create-task-form');
+  const inputData = document.getElementById('new-task-description')
+  const taskUl = document.getElementById('tasks')
+
+    newTaskForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      renderApp()
+    })
+
+    const addTask = () => {
+      taskUl.innerHTML = `<li>${inputData.value}</li>`
+    }
+
+    const renderApp = () => {
+      if(taskUl.innerHTML) {
+        addTask()
+      } else {
+          console.log('fail')
+      }
+      
+    }    
 });
